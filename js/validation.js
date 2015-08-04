03fa47b511036ae29f09b5955b22d5aeaa835db7
Renzo Westerbeek <renzowesterbeek@gmail.com>
2015-08-04 11:58:45 +0200

    Clean up code

--- js/validation.js
+++ js/validation.js
@@ -2,16 +2,32 @@
 // Validates inputfields on the client-side
 // Created on 4-8-15
 
-var inputErrors = [];
+var leerlingErrors = [];
+var emailErrors = [];
+
+function clearErrors(){
+  $("#errors").html("");
+}
+
+function addError(item){
+  var curHTML = $("#errors").html();
+  $("#errors").html(curHTML + "<li>"+item+"</li>");
+}
 
 // Intervents submit event and checks if InputErrors array is empty
 function validateInput(){
+  var inputErrors = leerlingErrors.concat(emailErrors);
+  clearErrors();
   if(inputErrors.length > 0){
     console.log("Some errors occured, form not submitted:");
     console.log(inputErrors);
+    inputErrors.forEach(function(item){
+      addError(item);
+    });
     return false;
   } else if($("#leerlingnum").val().length === 0 || $("#email").val().length === 0){
     console.log("Can't leave fields empty");
+    addError("Laat geen velden leeg");
     return false;
   } else {
     return true;
@@ -28,31 +44,40 @@ function changeColor(element, color){
 $(document).ready(function(){
   // Validate leerlingnummer inputfield
   $("#leerlingnum").keyup(function(){
-    inputErrors = []; // Clear errors when typing
-    changeColor($(this), 'green');
+    leerlingErrors = []; // Clear errors when typing
+    changeColor($(this), 'green'); // Green when no errors
 
-    if($(this).val().length !== 7){
-      changeColor($(this), 'red');
-      inputErrors.push("Incorrect leerlingnummer size");
+    if($(this).val().length < 7){
+      leerlingErrors.push("Leerlingnummer te kort");
+    }
+    if($(this).val().length > 7){
+      leerlingErrors.push("Leerlingnummer te lang");
     }
     if($(this).val().indexOf("O") !== 0){
+      leerlingErrors.push("Geen correct leerlingnummer ingevoerd");
+    }
+
+    // If there's an error, change border-color to red
+    if(leerlingErrors.length > 0){
       changeColor($(this), 'red');
-      inputErrors.push("Missing O");
     }
   });
 
   // Validate email inputfield
   $("#email").keyup(function(){
-    inputErrors = []; // Clear errors when typing
-    changeColor($(this), 'green');
+    emailErrors = []; // Clear errors when typing
+    changeColor($(this), 'green'); // Green when no errors
 
     if($(this).val().length < 7){
-      changeColor($(this), 'red');
-      inputErrors.push("Incorrect email size");
+      emailErrors.push("Email-addres te kort");
     }
     if($(this).val().indexOf("@") < 0 || $(this).val().indexOf(".") < 0){
+      emailErrors.push("Voer een juist email-adres in");
+    }
+
+    // If there's an error, change border-color to red
+    if(emailErrors.length > 0){
       changeColor($(this), 'red');
-      inputErrors.push("Email isn't correctly formatted");
     }
   });
 });
